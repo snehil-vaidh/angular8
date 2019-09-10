7342345b0f39bff0ec5135fecee3ab29f346571e
Kuhad <kamini.kuhad@accenture.com>
2019-09-10 10:46:25 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,44 +4,30 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `<body>
   <div class="container">
-  
-      <div class="row">
-          <div >
-          
-              <form class="form-inline">
-             
-                  <div class="form-row">
-                      <div class="form-group">
-                      <input class="insetreg" name="username" size="15" placeholder="  Username" type="text" name="username">
-                      <input name="password" type="password" size="15" placeholder="  Password" class="insetreg">
-                      <button class="loginbtn">Login</button>
-                      </div>
-                      
-                  </div>  
-                  
-              </form>
-          </div>
-      </div>
-  
-  
-</div><div>
-      <nav class="navbar navbar-inverse">
-          <div class="container-fluid">
-              
-              <ul class="nav navbar-nav">
-              <li><a [routerLink]="['/register']">SIGN UP</a></li>
-              <li><a [routerLink]="['/aboutus']">ABOUT US</a></li>
-              <li><a [routerLink]="['/how']">HOW IT WORKS</a></li>
-              <li><a [routerLink]="['/blogs']">BLOGS</a></li>
-              <li><a [routerLink]="['/career']">CAREER</a></li>
-              <li><a [routerLink]="['/contact']">CONTACT US</a></li>
-              </ul>
-          </div>
+    <form class="form-inline">
+        <input class="insetreg" name="username" size="15" placeholder="Username" type="text" name="username">&nbsp;
+        <input name="password" type="password" size="15" placeholder="Password" class="insetreg">&nbsp;
+        <button class="loginbtn">Login</button>
+    </form>
+    </div>
+    <div class="container">
+    <nav class="navbar navbar-inverse">
+        <ul class="nav navbar-nav">
+        <li><a [routerLink]="['/register']">SIGN UP</a></li>
+        <li><a [routerLink]="['/aboutus']">ABOUT US</a></li>
+        <li><a [routerLink]="['/how']">HOW IT WORKS</a></li>
+        <li><a [routerLink]="['/blogs']">BLOGS</a></li>
+        <li><a [routerLink]="['/career']">CAREER</a></li>
+        <li><a [routerLink]="['/contact']">CONTACT US</a></li>
+        </ul>
+         
       </nav>
-      <div class="container">
+     
+  </div>
+  <div class="container">
           <router-outlet></router-outlet>
       </div>
-  </div></body>
+      </body>
   `,
   styleUrls:['./app.component.css']
 })
